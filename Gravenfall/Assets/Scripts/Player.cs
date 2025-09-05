1fbcebed6477999e97fbbc69d3333eab630fabbb
Luiz Benevides <149179672+BenevidesLuiz@users.noreply.github.com>
2025-09-05 20:03:49 -0300

    versao1.0

---
+++ Gravenfall/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int live;
+    void Start(){
+        this.live = 100;
+    }
+
+    
+    void Update(){
+        
+    }
+}
