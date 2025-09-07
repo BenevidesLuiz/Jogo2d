667c766e0574d495ec8720ff8455d2f3fc5a5a20
Kaio Marques de Almeida <81613154+KaioMarquesdeAlmeida@users.noreply.github.com>
2025-09-07 19:47:11 -0400

    Movimentação com animacão(incompleta)

--- Gravenfall/Assets/Scripts/Player.cs
+++ Gravenfall/Assets/Scripts/Player.cs
@@ -1,15 +1,54 @@
+﻿using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 5f;
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+    private PlayerMove controls;
+    private Animator animator;
 
-    public int live;
-    void Start(){
-        this.live = 100;
+
+    void Awake()
+    {
+        controls = new PlayerMove();
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
     }
 
-    
-    void Update(){
-        
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    void FixedUpdate()
+    {
+     
+        rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
+        if (moveInput.x > 0)
+        {
+            animator.SetFloat("Speed", 7);
+        }
+        else{ 
+            animator.SetFloat("Speed", 0);
+
+        }
+
+        Debug.Log(moveInput.x);
+
     }
 }
